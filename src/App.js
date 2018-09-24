e6fdbf1a92c09cc701dda1d3383e490661fdb54a
neenus <neenus@live.ca>
2018-09-24 01:06:04 -0400

    api call now console.logs the search result

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 import './components/_table.css';
 
 // Import components
-// import SearchBar from './components/_search-bar.js';
+import SearchBar from './components/_search-bar.js';
 import Table from './components/_table.js';
 
 class App extends Component {
@@ -16,13 +16,17 @@ class App extends Component {
     super(props);
 
     // set initial state
-    this.state = {value: ''};
+    this.state = {
+      value: '',
+      isLoaded: false,
+      items: []
+    };
   }
 
-  // componentDidMount(){
-  //   console.log("compnentDidMount <App />");
-  //   // console.log(searchBox);
-  // }
+  componentDidMount(){
+    console.log("compnentDidMount");
+    // console.log(searchBox);
+  }
   render() {
     return (
       <div className="container">
@@ -30,29 +34,11 @@ class App extends Component {
           <h1 className="App-title">My GitHub Favourits</h1>
         </header>
           <div className="sidebar-left">
-          <form onSubmit={this.handleSubmit}>
-            <input 
-              name="searchBox" 
-              id="search-box" 
-              type="text" 
-              placeholder="Enter a GitHub repo name"
-              value={this.state.value}
-              onChange={this.handleChange}
-              >
-            </input>
-          {/* <input id="search-btn" type="submit" value="Search"/> */}
-          <input 
-            // onChange={this.handleChange} 
-            id="search-btn" 
-            name="search"
-            type='submit'
-            value='Search'>
-          </input>
-        </form>
-          {/* <SearchBar 
-            formSubmitHandler={this.handleSubmit} 
+          <SearchBar 
+            handleSubmit={this.handleSubmit}
+            handleChange={this.handleChange}
           >
-          </SearchBar> */}
+          </SearchBar>
           <div className="search-results">
           <Table></Table>
         </div>
@@ -78,8 +64,14 @@ class App extends Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
+    const api_url= 'https://api.github.com/search/repositories?q='
     const input = event.target.searchBox.value
     alert(input)
+    fetch(`${api_url}${input}`)
+    .then(blob => blob.json())
+    .then(response => {
+      console.log(response)
+    })
     this.setState({
       value: '',
     })
--- src/components/_search-bar.js
+++ src/components/_search-bar.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
 
-
 export default class SearchBar extends Component{
   render(){
     return(
       <div className="search-bar">
-        <form onSubmit={this.props.formSubmitHandler}>
+        <form onSubmit={this.props.handleSubmit}>
+            <input 
+              name="searchBox" 
+              id="search-box" 
+              type="text" 
+              placeholder="Enter a GitHub repo name"
+              value={this.props.value}
+              onChange={this.props.handleChange}
+              >
+            </input>
           <input 
-            name="searchBox" 
-            id="search-box" 
-            type="text" 
-            placeholder="Enter a GitHub repo name"
-            // onChange={this.props.searchBoxHandler}
-            >
-          </input>
-          {/* <input id="search-btn" type="submit" value="Search"/> */}
-          <button 
-            // onClick={this.props.formSubmitHandler} 
             id="search-btn" 
-            name="search">
-            Search
-          </button>
+            name="search"
+            type='submit'
+            value='Search'>
+          </input>
         </form>
 
       </div>
